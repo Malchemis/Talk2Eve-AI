6574fa5e479e9c95c07a6c817031a40cf890a17d
Malchemis <malchemis@proton.me>
2024-06-06 22:28:27 +0200

    small changes

--- chat-cyber.py
+++ chat-cyber.py
@@ -7,7 +7,7 @@ from utils import load_tokenizer, load_model_on_available_device
 def main():
     # model_id = 'mistralai/Mistral-7B-Instruct-v0.3'
     model_id = 'google/gemma-2b-it'
-    cache_dir = './models/'
+    cache_dir = '/media/malchemis/CYBERTRON/models'
     input_text = "What is a Drive-by Compromise attack? Respond concretely and concisely."
 
     logging.basicConfig(level=logging.ERROR)
@@ -23,18 +23,15 @@ def main():
         return
 
     try:
-        # Tokenize input text
         print("Tokenizing input text...")
         input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
 
-        # Generate response
         print("Generating response...")
         start = time.time()
         output = model.generate(input_ids, max_length=120, pad_token_id=tokenizer.eos_token_id)
         end = time.time()
-        # Decode and print response
-        response = tokenizer.decode(output[0], skip_special_tokens=True)
-        print(response)
+
+        print(response := tokenizer.decode(output[0], skip_special_tokens=True))
         print(f"Response generated in {end - start:.2f} seconds.")
     except Exception as e:
         print(f"Error during text generation: {e}")
--- translate.py
+++ translate.py
@@ -2,34 +2,31 @@ from langdetect import detect
 import torch
 from transformers import pipeline
 
+import os
 import logging
 import time
 
 
 def main():
-    # User Message
     input_text = "Qu'est-ce qu'une attaque Drive-by Compromise ? Répondez de façon concrète et concise."
-    # Get the language of the input text
+
     lang = detect(input_text)
     print(f"Detected language: {lang}")
-    # Define variables
+
     model_id = 'Helsinki-NLP/opus-mt-fr-en' if lang == 'fr' else 'Helsinki-NLP/opus-mt-en-fr'
     cache_dir = "./models"
 
     logging.basicConfig(level=logging.ERROR)
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    import os
     os.environ['TRANSFORMERS_CACHE'] = cache_dir
-    print(f"using {device}")
-    pipe = pipeline('translation', model=model_id, tokenizer=model_id, device=device)
+    print("using ", device := torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
 
+    pipe = pipeline('translation', model=model_id, tokenizer=model_id, device=device)
     try:
-        # Translate the input text
         print("Translating input text...")
         start = time.time()
         output = pipe(input_text)
         end = time.time()
-        # Print the translated text
+
         print(output[0]['translation_text'])
         print(f"Translation generated in {end - start:.2f} seconds.")
     except Exception as e:
